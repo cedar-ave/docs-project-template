ea3f7c1536b9c73e93d991d1dc3f6de4d36281ff
Marla Sowards <marla.sowards@healthcatalyst.com>
2018-02-26 20:44:35 -0700

    move location of font awesome

--- flip-pdf.sh
+++ flip-pdf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# By default, page 1 of the PDF output is a table of contents. There is no setting to make page 1 a title page. To work around that, make page 2 a title page. Then run this script, which flips page 1 and page 2.
+# By default, page 1 of the PDF output is a table of contents. There is no setting to make page 1 a title page. To work around that, make page 2 a title page. Then run this script, which flips page 1 and page 2 of the generated PDF. It does this for all PDFs in `_site_pdf/docs-project-template_pdf`.
 
 cd _site_pdf/docs_articles
 
